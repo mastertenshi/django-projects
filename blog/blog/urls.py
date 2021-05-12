0121ec7627d394e683f8226e8896fcea94082004
Angel Mitev <angelmitev@protonmail.com>
2021-05-12 23:41:21 +0200

    Add comments for urls app_name scope

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from .views import BlogDetailView, BlogListView
 
 
+# Adds scope for when using URL
+# ex. {% url 'blog:home' %}
+#     {% url 'blog:post_detail' %}
 app_name = 'blog'
+
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
