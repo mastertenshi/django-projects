041576093f9ef54c73560c984d7bc375698b34e8
Angel Mitev <angelmitev@protonmail.com>
2021-05-14 22:00:03 +0200

    Add login/out, signup functionality in news/

--- news/users/forms.py
+++ news/users/forms.py
@@ -4,15 +4,19 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
+FIELDS = ('username', 'email', 'age', )
+
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ['age']
+        # fields = UserCreationForm.Meta.fields + ('age')
+        fields = FIELDS
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta(UserChangeForm):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        # fields = UserChangeForm.Meta.fields
+        fields = FIELDS
