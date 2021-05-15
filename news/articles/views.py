982c05a89f285c851c729b0584f026637e52a647
Angel Mitev <angelmitev@protonmail.com>
2021-05-15 22:47:20 +0200

    Update login required for articles && can only edit/delete owned articles

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
+
 from django.urls import reverse_lazy, reverse
 
 from .models import Article
+
 
 
 class ArticleListView(ListView):
@@ -11,26 +17,44 @@ class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
 
 
-class ArticleDetailView(DetailView):
+class LoginRequired(LoginRequiredMixin):
+    login_url = 'login'
+
+
+class UserAuthorized(UserPassesTestMixin):
+
+    def test_func(self):
+        article = self.get_object()
+        return article.author == self.request.user
+
+
+class ArticleCreateView(LoginRequired, CreateView):
+    model = Article
+    template_name = 'articles/article_new.html'
+    # fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
+    # default success_url is detail view (somehow)
+    # success_url = reverse_lazy('article_list')
+
+    # Set author form field to current user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ArticleDetailView(LoginRequired, DetailView):
     model = Article
     template_name = 'articles/article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequired, UserAuthorized, UpdateView):
     model = Article
     fields = ('title', 'body')
     template_name = 'articles/article_edit.html'
     success_url = reverse_lazy('article_list')
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequired, UserAuthorized, DeleteView):
     model = Article
     template_name = 'articles/article_delete.html'
-    success_url = reverse_lazy('article_list')
-
-
-class ArticleCreateView(CreateView):
-    model = Article
-    template_name = 'articles/article_new.html'
-    fields = ('title', 'body', 'author')
     success_url = reverse_lazy('article_list')
--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'article_list'
 LOGOUT_REDIRECT_URL = 'home'
