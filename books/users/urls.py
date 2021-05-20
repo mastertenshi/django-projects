447c829c5d3a247c99c853c1da4402d3ba277861
Angel Mitev <angelmitev@protonmail.com>
2021-05-21 00:49:59 +0200

    Update login functionality in books project
    Now redirects from loginpage to homepage if the user is already logged in

--- books/users/urls.py
+++ books/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignupView
+from .views import SignupView, CustomLoginView
 
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
--- books/users/views.py
+++ books/users/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,7 @@ class SignupView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class CustomLoginView(LoginView):
+    redirect_authenticated_user = True
